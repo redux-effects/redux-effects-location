e5d2b2a42b069f11503cf7b61e381de3954a0952
Andrew Shaffer <darawk@gmail.com>
2015-09-12 00:51:38 -0700

    use new action style properly

--- src/index.js
+++ src/index.js
@@ -17,28 +17,28 @@ const types = ['GET_URL', 'SET_URL', 'BIND_URL']
 function locationMiddleware (wnd) {
   wnd = wnd || window
 
-  return ({dispatch}) => next => effect =>
-    types.indexOf(effect.type) !== -1
-      ? Promise.resolve(handle(wnd, dispatch, effect))
-      : next(effect)
+  return ({dispatch}) => next => action =>
+    types.indexOf(action.type) !== -1
+      ? Promise.resolve(handle(wnd, dispatch, action))
+      : next(action)
 }
 
 /**
  * Handler
  */
 
-function handle (wnd, dispatch, effect) {
-  switch (effect.type) {
+function handle (wnd, dispatch, action) {
+  switch (action.type) {
     case 'GET_URL':
       return wnd.location.pathname + wnd.location.search
     case 'SET_URL':
-      const url = wnd.location.origin + effect.value
-      effect.replace
+      const url = wnd.location.origin + action.value
+      action.replace
         ? location.replace(url)
         : location.assign(url)
       break
     case 'BIND_URL':
-      bindUrl({wnd}, url => dispatch(effect.update(url)))
+      bindUrl({wnd}, url => dispatch(action.update(url)))
       break
   }
 }
